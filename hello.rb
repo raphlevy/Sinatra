2d8c6994c932354732d678aefc9de637f0118356
raphlevy <raphlevy.perso@gmail.com>
2015-08-03 15:15:24 +0100

    Picture kitty

--- hello.rb
+++ hello.rb
@@ -1,9 +1,11 @@
 require 'sinatra'
 
 get '/' do
-  'hello!'
-end
+  "<div>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>"
+ end
 
 get '/secret' do
-	'This is a secret page'
+	'This is a secret pageee'
 end
